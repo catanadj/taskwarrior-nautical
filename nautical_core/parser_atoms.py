2e1d3b1f93cef88242e8b740ad2c7a0059d39151
catanadj <catalin.nadja@gmail.com>
2026-09-15 21:23:57 +0300

    Remove unused parser compatibility shims

--- nautical_core/parser_atoms.py
+++
@@ -1,2 +0,0 @@
-from .parsing.parser_atoms import *  # noqa: F401,F403
-# Compatibility shim: canonical implementation lives under nautical_core.parsing.
--- nautical_core/parser_dnf.py
+++
@@ -1,2 +0,0 @@
-from .parsing.parser_dnf import *  # noqa: F401,F403
-# Compatibility shim: canonical implementation lives under nautical_core.parsing.
